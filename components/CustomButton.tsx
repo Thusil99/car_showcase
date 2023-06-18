9e21caf756955e70068d8880d1b73d29410b52fd
thusil <thusilr@booleanlabs.biz>
2023-06-18 13:31:11 +0530

    Initial commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+import { type } from "os";
+
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn`}
+      onClick={() => {}}
+    ></button>
+  );
+};
+
+export default CustomButton;
